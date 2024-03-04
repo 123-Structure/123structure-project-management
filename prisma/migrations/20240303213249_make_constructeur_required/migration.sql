9258b4b101123b059789f12c8694fdec6fc2d346
Baptiste LECHAT <baptistelechat@outlook.fr>
2024-03-04 18:52:47 +0100

    🙈 (Migrations) - Add migrations folder into .gitignore

--- prisma/migrations/20240303213249_make_constructeur_required/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `constructeur` on table `Dossier` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "Dossier" ALTER COLUMN "constructeur" SET NOT NULL;
